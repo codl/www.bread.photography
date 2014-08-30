d579e4429189f615d577dbb7764b7e51ca5e3227
Corentin Delcourt <codl@codl.fr>
2014-08-30 22:13:09 +0200

    get image urls from flickr instead of building them
    this avoids getting images without a large (_b) version

--- bread.js
+++ bread.js
@@ -2,11 +2,18 @@ var photos = [];
 
 function getBreadPhotos(){
     $.ajax({
-        url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a845e09c486efec8e2259c05e034254&text=bread&format=json&nojsoncallback=1&sort=relevance&content_type=1",
+        url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a845e09c486efec8e2259c05e034254&text=bread&format=json&nojsoncallback=1&sort=relevance&content_type=1&extras=url_o,url_l",
         success: function(data){
             for(var i=0; i < data.photos.photo.length; i++){
                 var photo = data.photos.photo[i];
-                url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_b.jpg";
+                var url;
+                if(photo.url_o)
+                    url = photo.url_o;
+                else if(photo.url_l)
+                    url = photo.url_l;
+                else // an image for ants, skip it
+                    continue;
+
                 // preload in cache, if 200 then we can add it to the global photos array
                 var img = $("<img/>");
                 img.on("load", {url:url}, function(e){
