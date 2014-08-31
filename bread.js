7b0bdcd654e0b8f59a21d4244f31eb70650aeb91
Corentin Delcourt <codl@codl.fr>
2014-08-31 11:21:04 +0200

    add source link

--- bread.js
+++ bread.js
@@ -3,7 +3,7 @@ var started = false;
 
 function getBreadPhotos(){
     $.ajax({
-        url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a845e09c486efec8e2259c05e034254&text=bread&format=json&nojsoncallback=1&sort=relevance&content_type=1&extras=url_o,url_l&license=1,2,3,4,5,6,7",
+        url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0262b62c1e3dda3b26823024875e2bc1&text=bread&format=json&nojsoncallback=1&sort=relevance&content_type=1&extras=url_o,url_l&license=1,2,3,4,5,6,7",
         success: function(data){
             for(var i=0; i < data.photos.photo.length; i++){
                 var photo = data.photos.photo[i];
@@ -15,13 +15,13 @@ function getBreadPhotos(){
                 else // an image for ants, skip it
                     continue;
 
+                var link = "https://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
+
                 // preload in cache, if 200 then we can add it to the global photos array
                 var img = $("<img/>");
-                img.on("load", {url:url}, function(e){
-                    console.log("i load");
-                    var url = e.data.url;
-                    console.log(url);
-                    photos.push(url);
+                img.on("load", {url:url, link:link}, function(e){
+
+                    photos.push(e.data);
                     if(!started){
                         started = true;
                         showNext();
@@ -46,7 +46,7 @@ function showNext(){
 
     var stalebread = $(".bread");
     var newbread = $("<div class='new bread'/>");
-    newbread.css("background-image", "url("+photo+")");
+    newbread.css("background-image", "url("+photo.url+")");
     newbread.on("transitionend", transitionend);
     stalebread.removeClass("new");
     $(document.body).append(newbread);
@@ -55,6 +55,8 @@ function showNext(){
     var _ = newbread[0].offsetTop; //trigger layout
     newbread.removeClass("new");
     newbread.css("transform", randomTransform());
+
+    $("a#source").attr("href", photo.link);
 }
 
 function transitionend(e){
